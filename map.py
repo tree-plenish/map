5e4d0ae4cc46fdcce75954e7f81d56939d8600be
Joy He <joyhe208@gmail.com>
2021-01-25 12:18:34 -0500

    used python script to generate geojson with state frequency in schools

---
+++ map.py
@@ -0,0 +1,41 @@
+import folium
+from folium import *
+import pandas as pd
+import numpy as np
+import json
+import os
+
+spreadsheetPath = '/Users/joyhe208/desktop/tree-plenish/demographics/stats.html'
+
+demographics = pd.read_html(spreadsheetPath)[0]
+
+#next steps:
+#get a 2d array of state and #of schools
+#place these in geojson
+#create chloropleth map
+
+all_states = demographics.loc[:,'B'].drop([0],axis=0)
+
+state_frequency = {}
+
+for state in all_states:
+	if(state in state_frequency.keys()):
+		state_frequency[state] = state_frequency[state]+1
+	else:
+		state_frequency[state] = 1
+
+with open('states.json', 'r') as f:
+	states_geo = json.load(f)
+	for state in states_geo['features']:
+		if(state['properties']['NAME'] in state_frequency.keys()):
+			state['properties']['NUMSCHOOLS'] = state_frequency[state['properties']['NAME']]
+
+with open('newStates.json', 'w') as f:
+	json.dump(states_geo, f, indent=4)
+
+os.remove('states.json')
+os.rename('newStates.json', 'states.json')
+
+
+
+
