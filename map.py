66b72f59162560f393564f9ae703423062c124a1
Joy He <joyhe208@gmail.com>
2021-01-25 12:20:18 -0500

    clean up + documentation

--- map.py
+++ map.py
@@ -9,21 +9,19 @@ spreadsheetPath = '/Users/joyhe208/desktop/tree-plenish/demographics/stats.html'
 
 demographics = pd.read_html(spreadsheetPath)[0]
 
-#next steps:
-#get a 2d array of state and #of schools
-#place these in geojson
-#create chloropleth map
-
+#dataframe with just the states of each school
 all_states = demographics.loc[:,'B'].drop([0],axis=0)
 
 state_frequency = {}
 
+#generating dictionary with state and number of schools in that state
 for state in all_states:
 	if(state in state_frequency.keys()):
 		state_frequency[state] = state_frequency[state]+1
 	else:
 		state_frequency[state] = 1
 
+#adding the frequency variable from this 50 states json
 with open('states.json', 'r') as f:
 	states_geo = json.load(f)
 	for state in states_geo['features']:
